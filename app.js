58f9e9dce416264047f5aebd6ab6035499b12525
star1yy.dron <162572552+DRONCHIKnaLAKI@users.noreply.github.com>
2025-08-08 10:56:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-// Конфиг Firebase (замените на свой!)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "pk.eyJ1IjoiZHJvbmxveCIsImEiOiJjbWUyaTQ3YTAwbmVhMmpzYnV2bTZiaWQ2In0.S9mgehHOOA-z6X7yU2vxvQ",
-  authDomain: "active-ua.firebaseapp.com",
-  projectId: "active-ua",
-  storageBucket: "active-ua.appspot.com"
+  apiKey: "AIzaSyA1T5O-dktACGcUZvGbbPxQHcLY8Ljmo8Y",
+  authDomain: "activeua-87a2f.firebaseapp.com",
+  projectId: "activeua-87a2f",
+  storageBucket: "activeua-87a2f.firebasestorage.app",
+  messagingSenderId: "441222831178",
+  appId: "1:441222831178:web:b79cf76377bf5b444c9ab3",
+  measurementId: "G-JM1QN95H4Y"
 };
 
-// Инициализация Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Инициализация карты
 const map = L.map('app').setView([50.45, 30.52], 13);
