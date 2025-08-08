f2059c2de777a39a4fba2a18a0aeab882e551e9a
star1yy.dron <162572552+DRONCHIKnaLAKI@users.noreply.github.com>
2025-08-08 10:34:49 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+// Конфиг Firebase (замените на свой!)
+const firebaseConfig = {
+  apiKey: "AIzaSy...",
+  authDomain: "active-ua.firebaseapp.com",
+  projectId: "active-ua",
+  storageBucket: "active-ua.appspot.com"
+};
+
+// Инициализация Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+// Инициализация карты
+const map = L.map('app').setView([50.45, 30.52], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+// Загрузка данных
+db.collection("activities").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    const data = doc.data();
+    L.marker([data.coords.lat, data.coords.lng])
+      .addTo(map)
+      .bindPopup(`<b>${data.name}</b><br>${data.address}`);
+  });
+});
